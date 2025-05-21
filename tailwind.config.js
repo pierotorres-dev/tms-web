17963ed19c8539a43f254629fd37655a90c0eed0
Piero Torres <torresartica1701@gmail.com>
2025-05-21 12:10:38 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Rutas a todos tus archivos de plantillas y componentes de Angular
+  ],
+  theme: {
+    extend: {}, // Aquí puedes extender el tema base de Tailwind
+  },
+  plugins: [], // Aquí puedes añadir plugins de Tailwind
+};
