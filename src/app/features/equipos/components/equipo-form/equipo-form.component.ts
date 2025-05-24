2ed508b8ef94fbe582fd9ce5f1fc8dad549e0dc8
Piero Torres <torresartica1701@gmail.com>
2025-05-24 12:22:55 -0500

    refactor: refactor equipo-list component

---
+++ src/app/features/equipos/components/equipo-form/equipo-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipo-form',
+  imports: [],
+  templateUrl: './equipo-form.component.html',
+  styleUrl: './equipo-form.component.css'
+})
+export class EquipoFormComponent {
+
+}
---
+++ src/app/features/equipos/components/index.ts
@@ -0,0 +1,2 @@
+// Components
+export { EquipoFormComponent } from './equipo-form/equipo-form.component';
